ce333edc8a69306bf2ac02494c5670741184344c
Nramirez <nramirez@uma.es>
2025-05-05 16:24:58 +0200

    refactor(module): rename invalid name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ic',
+    name='initial_conditions',
     version='0.0',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
