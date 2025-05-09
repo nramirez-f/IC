487392fe17f88a730a359b3bdc414adf4e29a06e
Nramirez <nramirez@uma.es>
2025-05-09 11:37:14 +0200

    refactor(global): change repo to fit with numerica module

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='initial_conditions',
-    version='0.0',
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    install_requires=[
-    'numpy'
-    ],
+    name="initial_conditions",
+    version="0.0",
+    py_modules=["__init__", "scalar"],
+    install_requires=["numpy"],
     author="Nramirez",
-    description="Initial conditions for numerical methods",
+    description="Initial Conditions for Numerical Methods",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/nramirez-f/Initial-Conditions",
+    url="https://github.com/nramirez-f/IC",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
